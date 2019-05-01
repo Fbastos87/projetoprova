1f3c682ad9f8e49bc13b96d96e8d889df9ac3748
Fbastos87 <49735702+Fbastos87@users.noreply.github.com>
2019-05-01 13:21:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,66 @@
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title>Prova</title>
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-		<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" language="javascript">
-		$(document).ready(function() {
-			$('#listar-usuario').DataTable({			
-				"processing": true,
-				"serverSide": true,
-				"ajax": {
-					"url": "processo.php",
-					"type": "POST"
-				}
-			});
-		} );
-		</script>
-	</head>
-	<body>
-		<h1>Listar usuários</h1>
-		<table id="listar-usuario" class="display" style="width:100%">
-			<thead>
-				<tr>
-					<th align=left>Nome</th>
-					<th align=left>Data de Nascimento</th>
-					<th align=left>Salario</th>
-				</tr>
-			</thead>
-		</table>		
-	</body>
-	
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    
+    <title>Página de consulta</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css"
+     href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    
+    <script type="text/javascript" charset="utf8" 
+        src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    
+    <script type="text/javascript" charset="utf8" 
+      src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 
+    <script>
+        $(document).ready(function() {
+            $('#table_id').DataTable();
+        } );
+    </script>
 
+</head>
+<body>
+<br><br>
+<a class="btn btn-success btn-xs" href=incluir.php> Adicionar Pessoas</a>
+<br><br>
+
+<table id="table_id" class="display">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th> Nome</th>
+            <th>Data de Nascimento</th>
+			<th>Salário</th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+<?php
+$dsn = 'mysql:dbname=projeto;host=127.0.0.1';
+$user = 'root'; 
+$password = '';
+try {
+$dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+        $sql = 'SELECT * FROM cadastro';
+        foreach ($dbh->query($sql) as $row) {
+        echo "<tr>";
+        echo "<td align=left>". $row['id'] . "</td>";
+        echo "<td align=left>". $row['nome'] . "</td>";
+        echo "<td align=left>". $row['data_nasc'] . "</td>";
+		echo "<td align=left>". $row['salario'] . "</td>";?>
+		<td> <a class="btn btn-primary btn-xs" href=editar.php?id="<?php echo $row['id']?>">Editar</a> </td>
+		<td> <a class="btn btn-danger btn-xs" href=excluir.php?id="<?php echo $row['id']?>">Excluir</a> </td>
+		<?php
+        echo "</tr>";
+        }
+?>
+    </tbody>
+</table>    
+</body>
 </html>
-
