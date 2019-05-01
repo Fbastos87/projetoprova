c576ed14ede6737031f73de81a5f9d06fa5bd5ae
Fbastos87 <49735702+Fbastos87@users.noreply.github.com>
2019-05-01 14:04:28 -0300

    Create editar.php

---
+++ editar.php
@@ -0,0 +1,33 @@
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<?php
+$id=$_GET['id'];
+$dsn = 'mysql:dbname=projeto;host=127.0.0.1';
+$user = 'root'; //mysql usuario=root
+$password = '';//sem senha
+try {
+$dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+$sql = "SELECT * FROM cadastro where id=$id";
+foreach ($dbh->query($sql) as $row) {
+    echo "<form action=salvar_editar.php>";
+    echo "<p>Nome</p>";
+    echo "<p><input type=text name=nome value='".
+            $row['nome'] . "'>";
+    echo "<p>Data de Nascimento</p>";
+    echo "<p><input type=text name=data_nasc value=".
+            $row['data_nasc'] . ">";
+	echo "<p><input type=number name=salario value=".
+			$row['salario'] . ">";
+    echo "<input type=hidden name=id value=". 
+            $row['id'] . " >";
+    echo "<br><br>"?>
+	
+	<input class="btn btn-primary btn-xs" type=submit value=Salvar>
+	<?php
+echo "</form>";
+}
+echo "<br><br>
+<a href=index.php>Voltar</a> ";
+?>
