f3e9a1d863d3cac77c459cf4f03927cb4c1a6880
Fbastos87 <49735702+Fbastos87@users.noreply.github.com>
2019-05-01 14:18:19 -0300

    Update salvar.php

--- salvar.php
+++ salvar.php
@@ -1,4 +1,4 @@
-
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <?php
 $nome = $_GET['nome1'];
 $data_nasc = $_GET['data_nasc'];
@@ -13,6 +13,6 @@ echo 'Connection failed: ' . $e->getMessage();
 }
 $count = $dbh->exec("insert into cadastro(nome,data_nasc,salario) 
                 values('$nome', '$data_nasc' , '$salario') ");
-echo "<p>$count registro foi incluído</p>";
-echo "<br><br><a href=index.php>Voltar</a>  ";
-?>
+echo "<p>$count registro foi incluído</p>";?>
+<a class="btn btn-danger btn-xs" href=index.php>Voltar</a> 
+
